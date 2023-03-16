6099c5e91d65d1e87dfe6a4363e91608ca64dc69
closgarcia <112340103+closgarcia@users.noreply.github.com>
2023-03-16 15:32:03 -0700

    Lab 6

--- hw04.py
+++ hw04.py
@@ -125,7 +125,19 @@ def balanced(m):
     >>> check(HW_SOURCE_FILE, 'balanced', ['Index'])
     True
     """
-    "*** YOUR CODE HERE ***"
+    if is_planet(m):
+        return True
+    else:
+        assert is_mobile(m), "must get total weight of a mobile or a planet"
+        left_arm = left(m)
+        right_arm = right(m)
+        left_end = end(left_arm)
+        right_end = end(right_arm)
+        left_length = length(left_arm)
+        right_length = length(right_arm)
+        left_weight = total_weight(left_end)
+        right_weight = total_weight(right_end)
+        return left_length * left_weight == right_length * right_weight and balanced(left_end) and balanced(right_end)
 
 
 def totals_tree(m):
@@ -157,9 +169,11 @@ def totals_tree(m):
     >>> check(HW_SOURCE_FILE, 'totals_tree', ['Index'])
     True
     """
-    "*** YOUR CODE HERE ***"
+    
+        
+    
 
-
+    
 def replace_loki_at_leaf(t, lokis_replacement):
     """Returns a new tree where every leaf value equal to "loki" has
     been replaced with lokis_replacement.
@@ -189,8 +203,13 @@ def replace_loki_at_leaf(t, lokis_replacement):
     >>> laerad == yggdrasil # Make sure original tree is unmodified
     True
     """
-    "*** YOUR CODE HERE ***"
-
+    if is_leaf(t):
+        if label(t) == 'loki':
+            return tree(lokis_replacement)
+        else:
+            return t
+    else:
+        return tree(label(t), [replace_loki_at_leaf(b, lokis_replacement) for b in branches(t)])
 
 def has_path(t, word):
     """Return whether there is a path in a tree where the entries along the path
@@ -223,8 +242,8 @@ def has_path(t, word):
     False
     """
     assert len(word) > 0, 'no path for empty word.'
-    "*** YOUR CODE HERE ***"
-
+    if len(word) == 1:
+        return label(t) == word
 
 def str_interval(x):
     """Return a string representation of interval x."""
@@ -247,13 +266,14 @@ def interval(a, b):
 
 def lower_bound(x):
     """Return the lower bound of interval x."""
-    "*** YOUR CODE HERE ***"
+    return x[0]
+
+
 
 
 def upper_bound(x):
     """Return the upper bound of interval x."""
-    "*** YOUR CODE HERE ***"
-
+    return x[1]
 
 def str_interval(x):
     """Return a string representation of interval x."""
@@ -281,14 +301,15 @@ def mul_interval(x, y):
 def sub_interval(x, y):
     """Return the interval that contains the difference between any value in x
     and any value in y."""
-    "*** YOUR CODE HERE ***"
+    negative_y = interval(-upper_bound(y), -lower_bound(y))
+    return add_interval(x, interval(-upper_bound(y), -lower_bound(y)))
 
 
 def div_interval(x, y):
     """Return the interval that contains the quotient of any value in x divided by
     any value in y. Division is implemented as the multiplication of x by the
     reciprocal of y."""
-    "*** YOUR CODE HERE ***"
+    assert not (lower_bound(y) <= 0 <= upper_bound(y)), 'Divide by zero'
     reciprocal_y = interval(1 / upper_bound(y), 1 / lower_bound(y))
     return mul_interval(x, reciprocal_y)
 
@@ -313,8 +334,8 @@ def check_par():
     >>> lower_bound(x) != lower_bound(y) or upper_bound(x) != upper_bound(y)
     True
     """
-    r1 = interval(1, 1)  # Replace this line!
-    r2 = interval(1, 1)  # Replace this line!
+    r1 = interval(1, 2)  # Replace this line!
+    r2 = interval(3, 4)  # Replace this line!
     return r1, r2
 
 
